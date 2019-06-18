e394d12dfe45b38607089e0cb1eb363b62bd82ee
James Oliveira da Silva <jamesodas@gmail.com>
2019-06-18 00:00:18 -0300

    Configuring project

---
+++ tests/testing.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import main from '../src/main';
+
+describe('Testing...', () => {
+  it('shoult exist the main instance', () => {
+    expect(main).to.exist;
+  });
+});
