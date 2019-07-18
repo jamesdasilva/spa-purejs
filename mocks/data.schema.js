badade8d17cae3663030705bde2ec1057f7ac287
James da Silva <jamesodas@gmail.com>
2019-07-18 11:52:32 -0300

    Add API Mock

---
+++ mocks/data.schema.js
@@ -0,0 +1,12 @@
+module.exports = [{
+  id: "idAutoIncrement",
+  meta: {
+    author: "name",
+    title: "text",
+    url: "enum:css-tricks.com;medium.com;mika.el;jackymao.wix"
+  },
+  category: "enum:ux_ui;case_study;product_design;discussion",
+  comments: "integer:1;10",
+  created_at: "enum:1459857600;1459858600;1459857900",
+  upvotes: "integer:1;10"
+}];
