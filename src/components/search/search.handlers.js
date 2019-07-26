663ba7acc2701b8d698712b67cb2a73529cd8ce3
James da Silva <jamesodas@gmail.com>
2019-07-26 11:39:24 -0300

    Debugging in search

--- src/components/search/search.handlers.js
+++ src/components/search/search.handlers.js
@@ -1,7 +1,6 @@
 import updateSearchTerm from '../../helpers/update-search-term';
 import moveCursorToEnd from '../../helpers/move-cursor-to-end';
-import cleanFocus from '../../helpers/clean-focus';
-import setFocus from '../../helpers/set-focus';
+//import cleanFocus from '../../helpers/clean-focus';
 import generateParamsFetchLinks from '../../helpers/generate-params-fetch-links';
 import setIsFetching from '../../helpers/set-is-fetching';
 import clearIsFetching from '../../helpers/clear-is-fetching';
@@ -13,17 +12,13 @@ const searchHandlers = (document, store) => {
   document.querySelector('#search-term')
   .addEventListener('input', function(e){
     store.setState(updateSearchTerm(store.getState(), this.value), false);
-    console.log('1', store.getState().UI.searchFocus);
     store.setState(setIsFetching(store.getState()));
-    console.log('2', store.getState().UI.searchFocus);
       const paramers = generateParamsFetchLinks(store.getState());
       fetchLinks(paramers).then((newData) => {
         store.setState(setTotalCount(store.getState(), newData.count), false);
-        console.log('3', store.getState().UI.searchFocus);
         newData.links.then(links => {
           store.setState(setNewData({ ...store.getState() }, links));
           store.setState(clearIsFetching(store.getState()));
-          console.log('4', store.getState().UI.searchFocus);
         });
       });
   });
